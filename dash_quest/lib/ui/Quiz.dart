c57a7f36d30f804be090491c223ad9e26edd2ef6
Valeriana <valeria.do@gmail.com>
2019-06-01 11:50:29 -0500

    first commit

--- dash_quest/lib/ui/Quiz.dart
+++ dash_quest/lib/ui/Quiz.dart
@@ -1,4 +1,4 @@
 //Quiz UI widgets
 import 'package:flutter/material.dart';
 
-//Column -> Questions text -> -grid ->button 
+//Column -> Questions text -> -grid, children ()text, image, codesnipet, button ->button )
