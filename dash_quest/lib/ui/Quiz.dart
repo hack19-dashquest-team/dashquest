96efde193fd4902c37c38612ed4f5ba726dca4a9
Valeriana <valeria.do@gmail.com>
2019-06-01 12:25:01 -0500

    Quiz widget tree

--- dash_quest/lib/ui/Quiz.dart
+++ dash_quest/lib/ui/Quiz.dart
@@ -2,3 +2,40 @@
 import 'package:flutter/material.dart';
 
 //Column -> Questions text -> -grid, children ()txt, image, codesnipet, button ->button )
+
+class Quiz extends StatefulWidget {
+  @override
+  Widget build(BuildContext context) {
+return Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: <Widget>[
+Text("How advanced are you in Flutter?"),
+Expanded(
+  child: GridView.count(
+
+    crossAxisCount: 2,
+    children: <Widget>[
+Text("Text answer 1"),
+DecoratedBox( decoration: BoxDecoration(image: DecorationImage(image: AssetImage('.png'),),),),
+DecoratedBox( decoration: BoxDecoration(image: DecorationImage(image: AssetImage('.png'),),),),
+RaisedButton(child: const Text('Press Here'),
+ onPressed: () {
+//show more info? don't know yet.  
+},),
+
+    ],
+  ),
+),
+RaisedButton(child: const Text('Results'), 
+onPressed: () {
+//Route to results page
+},)
+
+  ],
+);
+
+}
+
+}
+
+
