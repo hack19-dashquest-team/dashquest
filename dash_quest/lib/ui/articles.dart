2793b455f9179a074df2323629ec27ae4f50a601
Valeriana <valeria.do@gmail.com>
2019-06-01 16:49:42 -0500

    Add image to articles

---
+++ dash_quest/lib/ui/articles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'Survey.dart';
+
+
+class Articles extends StatelessWidget {
+   @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(padding: const EdgeInsets.all(10.0),
+        child: Text("Learning Material"),),
+        Image.asset(
+              //change me
+              'images/end_survey.png',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+        // Expanded(
+        //  child: WebView(
+     
+        //     javascriptMode: JavascriptMode.unrestricted,
+        //     initialUrl: 'https://www.youtube.com/channel/UCDCFIqDZ1QUqivxVFQDxS0w',
+            
+        //   ),),
+        
+         SizedBox(
+            width: double.infinity,
+            child: RaisedButton(
+              child: const Text('Complete'),
+              onPressed: () {
+//Route to Survey page
+Navigator.push(context, MaterialPageRoute(builder: (context) => Survey()),);
+              },
+            )),
+      ],
+    );
+  }
+}
