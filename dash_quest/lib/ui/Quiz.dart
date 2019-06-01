1bb1f7d91e345c0d0d8ed668d9bb7cc1d386dc39
Valeriana <valeria.do@gmail.com>
2019-06-01 14:53:30 -0500

    Quiz class and Learn class

--- dash_quest/lib/ui/Quiz.dart
+++ dash_quest/lib/ui/Quiz.dart
@@ -1,41 +1,53 @@
 //Quiz UI widgets
 import 'package:flutter/material.dart';
 
-//Column -> Questions text -> -grid, children ()txt, image, codesnipet, button ->button )
-
-class Quiz extends StatefulWidget {
+class Quiz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-return Column(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: <Widget>[
-Text("How advanced are you in Flutter?"),
-Expanded(
-  child: GridView.count(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(padding: const EdgeInsets.all(10.0),
+        child: Text("How advanced are you in Flutter?"),),
+        Expanded(
+          child: GridView.count(
+            crossAxisCount: 2,
+            children: <Widget>[
+              MaterialButton(child: Text("Text answer 1"), onPressed: () {
 
-    crossAxisCount: 2,
-    children: <Widget>[
-Text("Text answer 1"),
-DecoratedBox( decoration: BoxDecoration(image: DecorationImage(image: AssetImage('.png'),),),),
-DecoratedBox( decoration: BoxDecoration(image: DecorationImage(image: AssetImage('.png'),),),),
-RaisedButton(child: const Text('Press Here'),
- onPressed: () {
-//show more info? don't know yet.  
-},),
+              }),
+              MaterialButton(
+                child: Image.asset(
+                  'images/ButtonExpand.jpg'),
+                onPressed: () {
 
-    ],
-  ),
-),
-RaisedButton(child: const Text('Results'), 
-onPressed: () {
+                },
+              ),
+              MaterialButton(
+                child: Image.asset(
+                   'images/ButtonExpand.jpg'),
+                onPressed: () {
+
+                },
+              ),
+              MaterialButton(
+                child: RaisedButton(child: const Text('Press Here')),
+                onPressed: () {
+//pressed grid cell .
+                },
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+            width: double.infinity,
+            child: RaisedButton(
+              child: const Text('Results'),
+              onPressed: () {
 //Route to results page
-},)
-
-  ],
-);
-
+              },
+            )),
+      ],
+    );
+  }
 }
-
-}
-
-
---
+++ dash_quest/lib/ui/learn.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Learn extends StatelessWidget {
+   @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(padding: const EdgeInsets.all(10.0),
+        child: Text("Learn"),),
+        Expanded(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.map),
+                title: Text('Learn to Build a Gradient Background'),
+                sub Text('UI'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
